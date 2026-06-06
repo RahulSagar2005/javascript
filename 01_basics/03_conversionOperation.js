b35e9de19d039faf4bebb5bdb6d9d08cfb4b8483
Rahul Sagar <rahulsagarxgr2@gmail.com>
2026-06-06 17:43:07 +0530

    practise

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score="33"
+console.log(typeof(score)) // string 
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber)) // number
+console.log(valueInNumber) // 33 
+
+// "33" =>33 
+// "33abc"=> vakue is NaN but type is number 
+// true=>1; flase=>0 
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=> true ; 0=> flase 
+// ""=> false 
+// "rahul"=> true 
+
+let someNumber=3
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
