7c1ac3e9e13db0cd7c6a5a8c0a07eac44cf178b9
Rahul Sagar <rahulsagarxgr2@gmail.com>
2026-06-07 14:57:59 +0530

    added operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,8 @@
 let score="33"
-console.log(typeof(score)) // string 
+// console.log(typeof(score)) // string 
 let valueInNumber=Number(score)
-console.log(typeof(valueInNumber)) // number
-console.log(valueInNumber) // 33 
+// console.log(typeof(valueInNumber)) // number
+// console.log(valueInNumber) // 33 
 
 // "33" =>33 
 // "33abc"=> vakue is NaN but type is number 
@@ -10,7 +10,7 @@ console.log(valueInNumber) // 33
 
 let isLoggedIn=1;
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1=> true ; 0=> flase 
 // ""=> false 
@@ -18,5 +18,37 @@ console.log(booleanIsLoggedIn)
 
 let someNumber=3
 let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber) 
+
+//************************ Operations ******************/
+let value=3 
+let negValue=-value 
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3) 
+let str1="Rahul"
+let str2=" Sagar" 
+let str3=str1+str2 
+// console.log(str3) 
+// console.log("1"+2)
+// console.log(1+"2") 
+// console.log("1"+2+2) 
+// console.log(1+2+"2") 
+// console.log((3+2)*5%2) 
+
+// console.log(true) 
+// console.log(+true)
+// console.log(+"") 
+let num1,num2,num3 
+num1=num2=num3=2+3 
+// console.log(num3)
+let gameCounter=100 
+++gameCounter
+console.log(gameCounter) 
+
+//postfix and infix js mdn 
