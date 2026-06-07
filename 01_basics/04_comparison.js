d0e61fdeb1dbfb372ad381136cdf0c855ed0053d
Rahul Sagar <rahulsagarxgr2@gmail.com>
2026-06-07 15:26:45 +0530

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2>1) 
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+// console.log("2">1) 
+// console.log("02">1) 
+// compare same datatypes 
+// console.log(null>0) 
+// console.log(null==0)
+// console.log(null>=0) 
+// console.log(undefined==0)
+// console.log(undefined>0)
+// console.log(undefined<0)
+// console.log(undefined>=0) 
+
+// strict check we use === triple assigment tht check value as well as datatype 
+// console.log("2"==2) in double equalls to conversion occurs 
+console.log("3"===3)
